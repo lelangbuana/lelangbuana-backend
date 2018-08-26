4ff4627c82b103ec2daae3a678d5f360f92e98f9
M. Alif Raher <alifraher@gmail.com>
2018-08-26 23:50:33 +0700

    Adding models.sequelize

--- index.js
+++ index.js
@@ -4,39 +4,43 @@
  * Module dependencies.
  */
 
-var app = require('./app');
-var debug = require('debug')('lelangbuana-backend:server');
-var http = require('http');
+const app = require('./app');
+const debug = require('debug')('lelangbuana-backend:server');
+const http = require('http');
+const models = require('./models');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
-server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
-server.on('error', onError);
-server.on('listening', onListening);
+models.sequelize.sync().then(function () {
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${server.address().port}`);
+    debug(`Server is listening on port ${server.address().port}`);
+  });
+  server.on('error', onError);
+  server.on('listening', onListening);
+})
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -60,9 +64,9 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  const bind = typeof port === 'string' ?
+    'Pipe ' + port :
+    'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -84,9 +88,9 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
+  const addr = server.address();
+  const bind = typeof addr === 'string' ?
+    'pipe ' + addr :
+    'port ' + addr.port;
   debug('Listening on ' + bind);
 }
