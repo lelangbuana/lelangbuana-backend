d52ca889d9d10631f7ad0cd5e807f843cea87343
M. Alif Raher <alifraher@gmail.com>
2018-08-27 15:22:19 +0700

    Improve message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ message: 'Welcome to REST API' });
+router.get('/', function (req, res, next) {
+  res.send({
+    message: 'Welcome to REST API',
+    endpoints: {
+      users: '/users'
+    }
+  });
 });
 
 module.exports = router;
