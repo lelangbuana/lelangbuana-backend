5fd2e49c092ab3547db146eb09a9a3ba87c53645
M. Alif Raher <alifraher@gmail.com>
2018-08-28 13:49:14 +0700

    Create controller for checkToken

---
+++ routes/auth/controller.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+
+const controller = {
+    get: async (req, res, next) => {
+        res.status(200).send({
+            message: `Token is valid!`
+        })
+    },
+
+    checkToken: async (req, res, next) => {
+        const withToken =
+            req.headers.authorization &&
+            req.headers.authorization.split(' ')[0] === 'Bearer'
+
+        if (withToken) {
+            const token = req.headers.authorization.split(' ')[1] || ''
+
+            jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+                if (error) {
+                    res.status(400).send({
+                        message: `Token is invalid when verifying`
+                    })
+                }
+                req.decoded = decoded
+                next()
+            })
+        } else {
+            res.status(400).send({
+                message: `Token is not found in request headers`
+            })
+        }
+    }
+}
+
+module.exports = controller
