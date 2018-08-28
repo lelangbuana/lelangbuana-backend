8d829bd40aa807eb3154a2ef0d3ef7cd6d6ba11f
M. Alif Raher <alifraher@gmail.com>
2018-08-28 13:53:32 +0700

    Adding checkToken, register, login and logout router

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('./controller');
+const authController = require('../auth/controller')
 
-router.get('/', controller.getAllUsers);
-router.get('/id/:id', controller.getOneUserById);
-router.get('/:username', controller.getOneUserByUsername);
-router.get('/search', controller.searchUsersByUsername);
-router.post('/', controller.createUser);
-router.put('/:id', controller.updateUser);
-router.delete('/:id', controller.deleteUser);
+router.get('/', controller.getAllUsers); // GET All Users
+router.get('/search', controller.searchUsersByUsername); // SEARCH Users By Username
+router.get('/id/:id', controller.getOneUserById); // GET One Users By ID 
+router.get('/checktoken', authController.checkToken, authController.get); // GET Token
+router.get('/logout', controller.logout); // LOGOUT
+router.get('/:username', controller.getOneUserByUsername); // GET One Users By Username
+
+router.post('/register', controller.register); // REGISTER User
+router.post('/login', controller.login); // LOGIN User
+
+router.put('/:id', controller.updateUser); // UPDATE User
+router.delete('/:id', controller.deleteUser); // DELETE User
 
 module.exports = router;
