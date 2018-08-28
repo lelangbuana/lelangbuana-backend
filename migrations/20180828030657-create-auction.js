c3f98c57d5fa6c8e02637e0deef8b0a224231b20
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-08-28 10:19:41 +0700

    Create migration file for 'auctions' table

---
+++ migrations/20180828030657-create-auction.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('auctions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('auctions');
+  }
+};
