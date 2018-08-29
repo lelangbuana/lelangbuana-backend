887422fbecfa4b1a8b6d7ee8f9a7e4260036fec0
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-08-29 15:07:35 +0700

    Add API to 'bids' model for getting data by auction id

--- routes/bids/index.js
+++ routes/bids/index.js
@@ -11,4 +11,6 @@ router.get('/:id', controller.getBidById)
 
 router.get('/user_id/:id', controller.getBidByUserId)
 
+router.get('/auction_id/:id', controller.getBidByAuctionId)
+
 module.exports = router
