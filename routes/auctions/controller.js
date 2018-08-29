6adecb49022fd43e00e2682664582cb637b9ab39
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-08-29 11:51:42 +0700

    Add API to 'auctions' model for posting new data

--- routes/auctions/controller.js
+++ routes/auctions/controller.js
@@ -18,6 +18,33 @@ const controller = {
             .catch(error => {
                 res.status(400).send({error})
             })
+    },
+
+    post: (req, res, next) => {
+        auction
+            .create({
+                user_id: req.body.user_id,
+                title: req.body.title,
+                item_condition: req.body.item_condition,
+                item_description: req.body.item_description,
+                quantity: req.body.quantity,
+                start_bid: req.body.start_bid,
+                max_bid: req.body.max_bid,
+                min_bid: req.body.min_bid,
+                bids_multiply: req.body.bids_multiply,
+                start_date: req.body.start_date,
+                end_date: req.body.end_date,
+                item_photo: req.body.item_photo,
+                status: req.body.status,
+                created_at: new Date()
+            })
+            .then(newAuction => {
+                res.send({newAuction})
+                console.log('You have sent a new auction data!')
+            })
+            .catch(error => {
+                res.status(400).send({error})
+            })
     }
 }
 
