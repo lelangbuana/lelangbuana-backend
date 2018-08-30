8d1d21343ef72436906405db174c10eff500b6bd
M. Alif Raher <alifraher@gmail.com>
2018-08-31 03:00:12 +0700

    Change router function name

--- routes/users/index.js
+++ routes/users/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const controller = require('./controller');
 const authController = require('../auth/controller')
 
-router.get('/', controller.getAllUsers); // GET All Users
-router.get('/search', controller.searchUsersByUsername); // SEARCH Users By Username
-router.get('/id/:id', controller.getOneUserById); // GET One Users By ID 
+router.get('/', controller.get); // GET All Users
+router.get('/search', controller.searchUserByUsername); // SEARCH User By Username
+router.get('/id/:id', controller.getUserById); // GET User By ID 
 router.get('/checktoken', authController.checkToken, authController.get); // GET Token
 router.get('/logout', controller.logout); // LOGOUT
-router.get('/:username', controller.getOneUserByUsername); // GET One Users By Username
+router.get('/:username', controller.getUserByUsername); // GET User By Username
 
 router.post('/register', controller.register); // REGISTER User
 router.post('/login', controller.login); // LOGIN User
