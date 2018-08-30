56e41fde3861df74c07a9e142a4c33e967ca369d
M. Alif Raher <alifraher@gmail.com>
2018-08-31 02:58:52 +0700

    Adding put and delete router for bid data

--- routes/bids/index.js
+++ routes/bids/index.js
@@ -4,13 +4,14 @@ const router = express.Router()
 const controller = require('./controller.js')
 
 /* ROUTES/VIEW */
-router.get('/', controller.get)
-router.post('/', controller.post)
+router.get('/', controller.get) // GET All Bids Data
+router.get('/:id', controller.getBidById) // GET Bid Data By ID 
+router.get('/user_id/:id', controller.getBidByUserId) // GET Bid Data By User ID
+router.get('/auction_id/:id', controller.getBidByAuctionId) // GET Bid Data By Auction ID
 
-router.get('/:id', controller.getBidById)
+router.post('/', controller.post) // POST Bid Data
 
-router.get('/user_id/:id', controller.getBidByUserId)
-
-router.get('/auction_id/:id', controller.getBidByAuctionId)
+router.put('/:id', controller.updateBid) // UPDATE Bid Data
+router.delete('/:id', controller.deleteBid) // DELETE Bid Data
 
 module.exports = router
