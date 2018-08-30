0ab4f045bd446788999ef8097cdaf75feb8e6d94
M. Alif Raher <alifraher@gmail.com>
2018-08-31 02:57:41 +0700

    Adding put and delete router for auction data

--- routes/auctions/index.js
+++ routes/auctions/index.js
@@ -4,13 +4,14 @@ const router = express.Router()
 const controller = require('./controller.js')
 
 /* ROUTES/VIEW */
-router.get('/', controller.get)
-router.post('/', controller.post)
+router.get('/', controller.get) // GET All Auction Data
+router.get('/:id', controller.getAuctionById) // GET Auction Data By ID 
+router.get('/user_id/:id', controller.getAuctionByUserId) // GET Auction Data By User ID
+router.get('/status/:status', controller.getAuctionByStatus) // GET Auction Data By Status
 
-router.get('/:id', controller.getAuctionById)
+router.post('/', controller.post) // POST Auction Data
 
-router.get('/user_id/:id', controller.getAuctionByUserId)
-
-router.get('/status/:status', controller.getAuctionByStatus)
+router.put('/:id', controller.updateAuction) // UPDATE Auction Data
+router.delete('/:id', controller.deleteAuction) // DELETE Auction Data
 
 module.exports = router
