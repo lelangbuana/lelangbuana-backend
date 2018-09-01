d711c30944b03c7b751f2fa4fc189e2bcd06094f
M. Alif Raher <alifraher@gmail.com>
2018-09-01 07:07:41 +0700

    Fixing API testing using GET method

--- test/auctions.test.js
+++ test/auctions.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /auctions', function () {
-    it('Respond with object users data', function () {
+    it('Respond with object auctions data', function () {
         return request(app)
             .get('/auctions')
             .set('Accept', 'application/json')
@@ -11,20 +11,20 @@ describe('GET /auctions', function () {
     })
 })
 
-describe('GET /auctions/:id', function () {
-    it('Respond with object users data', function () {
+describe('GET /auctions/:auction_id', function () {
+    it('Respond with object auctions data by auction_id', function () {
         return request(app)
-            .get('/auctions/:id')
+            .get('/auctions/1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
     })
 })
 
-describe('GET /auctions/user_id/:id', function () {
-    it('Respond with object users data', function () {
+describe('GET /auctions/user_id/:user_id', function () {
+    it('Respond with object auctions data by user_id', function () {
         return request(app)
-            .get('/auctions/user_id/:id')
+            .get('/auctions/user_id/1')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
@@ -32,11 +32,41 @@ describe('GET /auctions/user_id/:id', function () {
 })
 
 describe('GET /auctions/status/:status', function () {
-    it('Respond with object users data', function () {
+    it('Respond with object auctions data by status', function () {
         return request(app)
-            .get('/auctions/status/:status')
+            .get('/auctions/status/ongoing')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
     })
 })
+
+// describe('POST /auctions', function () {
+//     it('Respond with object auctions data', function() {
+//         return request(app)
+//         .post('/auctions')
+//         .set('Accept', 'application/json')
+//         .expect('Content-Type', /json/)
+//         .expect(200)
+//     })
+// })
+
+// describe('PUT /auctions/:auction_id', function () {
+//     it('Respond with object auctions data by auction_id', function() {
+//         return request(app)
+//         .put('/auctions/1')
+//         .set('Accept', 'application/json')
+//         .expect('Content-Type', /json/)
+//         .expect(200)
+//     })
+// })
+
+// describe('DELETE /auctions/:auction_id', function () {
+//     it('Respond with object auctions data by auction_id', function() {
+//         return request(app)
+//         .delete('/auctions/1')
+//         .set('Accept', 'application/json')
+//         .expect('Content-Type', /json/)
+//         .expect(200)
+//     })
+// })
