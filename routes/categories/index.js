ad28ccd1162e6823a9003598f8e5b6f516cbc728
Maya Sopiah Lubis <mayasopie275@gmail.com>
2018-09-05 23:38:33 +0700

    Add 'categories' GET API by parent category id

--- routes/categories/index.js
+++ routes/categories/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const controller = require('./controller.js')
 
 router.get('/', controller.get)
+router.get('/parent_id/:parent_category_id', controller.getByParentId)
 
 module.exports = router;
