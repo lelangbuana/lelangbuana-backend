40e4b1b5be3d1bd3b53a51203eae33fe3696eb02
M. Alif Raher <alifraher@gmail.com>
2018-09-05 15:25:30 +0700

    Add photo pruduct

--- seeders/2-demo-categories.js
+++ seeders/2-demo-categories.js
@@ -12,75 +12,256 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-   const demoCategories = [
-      {
+    const demoCategories = [{
+        category_id: 1,
         parent_category_id: null,
-        category_name: 'Computer',
+        category_name: 'Fashions',
         created_at: new Date()
       },
       {
+        category_id: 2,
         parent_category_id: null,
-        category_name: 'Electronic',
+        category_name: 'Furnitures',
         created_at: new Date()
       },
       {
+        category_id: 3,
         parent_category_id: null,
-        category_name: 'Book and Magazine',
+        category_name: 'Sports',
         created_at: new Date()
       },
       {
+        category_id: 4,
         parent_category_id: null,
-        category_name: 'Fashion',
+        category_name: 'Electronics',
         created_at: new Date()
       },
       {
+        category_id: 5,
         parent_category_id: null,
-        category_name: 'Kitchen and Cooking',
+        category_name: 'Vehicles',
         created_at: new Date()
       },
       {
+        category_id: 6,
+        parent_category_id: null,
+        category_name: 'Collections & Hoby',
+        created_at: new Date()
+      },
+      {
+        category_id: 7,
         parent_category_id: 1,
-        category_name: 'Laptop',
+        category_name: 'Clothes',
         created_at: new Date()
       },
       {
+        category_id: 8,
         parent_category_id: 1,
-        category_name: 'PC',
+        category_name: 'Watches',
         created_at: new Date()
       },
       {
+        category_id: 9,
         parent_category_id: 1,
-        category_name: 'Netbook',
+        category_name: 'Bags',
         created_at: new Date()
       },
       {
+        category_id: 10,
+        parent_category_id: 1,
+        category_name: 'Accessories',
+        created_at: new Date()
+      },
+      {
+        category_id: 11,
+        parent_category_id: 1,
+        category_name: 'Others',
+        created_at: new Date()
+      },
+      {
+        category_id: 12,
+        parent_category_id: 2,
+        category_name: 'Tables',
+        created_at: new Date()
+      },
+      {
+        category_id: 13,
+        parent_category_id: 2,
+        category_name: 'Chairs',
+        created_at: new Date()
+      },
+      {
+        category_id: 14,
+        parent_category_id: 2,
+        category_name: 'Cupboards',
+        created_at: new Date()
+      },
+      {
+        category_id: 15,
+        parent_category_id: 2,
+        category_name: 'Kitchen Equipments',
+        created_at: new Date()
+      },
+      {
+        category_id: 16,
+        parent_category_id: 2,
+        category_name: 'Others',
+        created_at: new Date()
+      },
+      {
+        category_id: 17,
         parent_category_id: 3,
-        category_name: 'Science-Fiction',
+        category_name: 'Bikes',
         created_at: new Date()
       },
       {
+        category_id: 18,
         parent_category_id: 3,
-        category_name: 'Non-Fiction',
+        category_name: 'Accessories',
         created_at: new Date()
       },
       {
-        parent_category_id: 4,
+        category_id: 19,
+        parent_category_id: 3,
+        category_name: 'Rackets',
+        created_at: new Date()
+      },
+      {
+        category_id: 20,
+        parent_category_id: 3,
+        category_name: 'Balls',
+        created_at: new Date()
+      },
+      {
+        category_id: 21,
+        parent_category_id: 3,
         category_name: 'Shoes',
         created_at: new Date()
       },
       {
+        category_id: 22,
+        parent_category_id: 3,
+        category_name: 'Jerseys',
+        created_at: new Date()
+      },
+      {
+        category_id: 23,
+        parent_category_id: 3,
+        category_name: 'Others',
+        created_at: new Date()
+      },
+      {
+        category_id: 24,
+        parent_category_id: 4,
+        category_name: 'Handphones & Tablets',
+        created_at: new Date()
+      },
+      {
+        category_id: 25,
+        parent_category_id: 4,
+        category_name: 'Cameras & Photography',
+        created_at: new Date()
+      },
+      {
+        category_id: 26,
+        parent_category_id: 4,
+        category_name: 'PC & Laptops',
+        created_at: new Date()
+      },
+      {
+        category_id: 27,
+        parent_category_id: 4,
+        category_name: 'TV & Monitors',
+        created_at: new Date()
+      },
+      {
+        category_id: 28,
+        parent_category_id: 4,
+        category_name: 'Others',
+        created_at: new Date()
+      },
+      {
+        category_id: 29,
         parent_category_id: 5,
-        category_name: 'Bottle',
+        category_name: 'Cars',
         created_at: new Date()
       },
       {
-        parent_category_id: 2,
-        category_name: 'Smartphone',
+        category_id: 30,
+        parent_category_id: 5,
+        category_name: 'Motorcycles',
         created_at: new Date()
       },
       {
-        parent_category_id: 2,
-        category_name: 'Iron',
+        category_id: 31,
+        parent_category_id: 5,
+        category_name: 'Spareparts',
+        created_at: new Date()
+      },
+      {
+        category_id: 32,
+        parent_category_id: 5,
+        category_name: 'Wheels',
+        created_at: new Date()
+      },
+      {
+        category_id: 33,
+        parent_category_id: 5,
+        category_name: 'Accessories',
+        created_at: new Date()
+      },
+      {
+        category_id: 34,
+        parent_category_id: 6,
+        category_name: 'Gem Stone',
+        created_at: new Date()
+      },
+      {
+        category_id: 35,
+        parent_category_id: 6,
+        category_name: 'Antiques',
+        created_at: new Date()
+      },
+      {
+        category_id: 36,
+        parent_category_id: 6,
+        category_name: 'Musical Instruments',
+        created_at: new Date()
+      },
+      {
+        category_id: 37,
+        parent_category_id: 6,
+        category_name: 'Dolls and Toys',
+        created_at: new Date()
+      },
+      {
+        category_id: 38,
+        parent_category_id: 6,
+        category_name: 'Tapes, Books & Magazines',
+        created_at: new Date()
+      },
+      {
+        category_id: 39,
+        parent_category_id: 6,
+        category_name: 'Handicrafts',
+        created_at: new Date()
+      },
+      {
+        category_id: 40,
+        parent_category_id: 6,
+        category_name: 'Artworks',
+        created_at: new Date()
+      },
+      {
+        category_id: 41,
+        parent_category_id: 6,
+        category_name: 'Old Money',
+        created_at: new Date()
+      },
+      {
+        category_id: 42,
+        parent_category_id: 6,
+        category_name: 'Others',
         created_at: new Date()
       }
     ];
@@ -95,6 +276,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('Person', null, {});
     */
-   return queryInterface.bulkDelete('categories', null, {});
+    return queryInterface.bulkDelete('categories', null, {});
   }
 };
--- seeders/4-demo-bids.js
+++ seeders/4-demo-bids.js
@@ -17,24 +17,28 @@ module.exports = {
         auction_id: 1,
         user_id: 2,
         bids_nominal: 1200000,
+        status: "Lose",
         created_at: new Date('August 28, 2018 12:00:00')
       },
       {
         auction_id: 1,
         user_id: 4,
         bids_nominal: 1350000,
+        status: "Lose",
         created_at: new Date('August 28, 2018 13:30:00')
       },
       {
         auction_id: 1,
         user_id: 3,
         bids_nominal: 1500000,
+        status: "Lose",
         created_at: new Date('August 28, 2018 15:30:00')
       },
       {
         auction_id: 1,
         user_id: 4,
         bids_nominal: 1700000,
+        status: "Lose",
         created_at: new Date('August 28, 2018 18:00:00')
       },
       {
@@ -48,62 +52,70 @@ module.exports = {
         auction_id: 2,
         user_id: 3,
         bids_nominal: 70000,
+        status: "Win",
         created_at: new Date('August 28, 2018 11:40:00')
       },
       {
         auction_id: 2,
         user_id: 1,
         bids_nominal: 90000,
+        status: "Lose",
         created_at: new Date('August 28, 2018 12:40:00')
       },
       {
         auction_id: 2,
         user_id: 3,
         bids_nominal: 100000,
+        status: "Lose",
         created_at: new Date('August 28, 2018 13:40:00')
       },
       {
         auction_id: 2,
         user_id: 4,
         bids_nominal: 130000,
+        status: "Lose",
         created_at: new Date('August 28, 2018 15:40:00')
       },
       {
         auction_id: 2,
         user_id: 1,
         bids_nominal: 140000,
-        status: "Win",
+        status: "Lose",
         created_at: new Date('August 28, 2018 15:40:00')
       },
       {
         auction_id: 3,
         user_id: 4,
         bids_nominal: 100000,
+        status: "Lose",
         created_at: new Date('August 10, 2018 10:20:00')
       },
       {
         auction_id: 3,
         user_id: 2,
         bids_nominal: 140000,
+        status: "Win",
         created_at: new Date('August 10, 2018 12:20:00')
       },
       {
         auction_id: 3,
         user_id: 1,
         bids_nominal: 150000,
+        status: "Lose",
         created_at: new Date('August 10, 2018 14:20:00')
       },
       {
         auction_id: 3,
         user_id: 2,
         bids_nominal: 200000,
+        status: "Lose",
         created_at: new Date('August 10, 2018 20:20:00')
       },
       {
         auction_id: 3,
         user_id: 4,
         bids_nominal: 280000,
-        status: "Win",
+        status: "Lose",
         created_at: new Date('August 10, 2018 20:20:00')
       },
     ];
